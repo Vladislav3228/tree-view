a2d1b30ebf53c20cb8cd96a9843ce1b8abe82512
Vladislav <33334984+Vladislav3228@users.noreply.github.com>
2019-11-12 17:18:02 +0800

    Update Trees.py

--- Trees/Trees/Trees.py
+++ Trees/Trees/Trees.py
@@ -9,27 +9,24 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 dbconfig = read_db_config()
 
-def create():
-    column_names = []
-    tableName = 'closures'
-
-    cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = " + "'" + tableName + "'")
-    column = cursor.fetchone()
-    while column is not None:
-        column_names.append(column[0])
+def create_Closures(tableName):
+    if tableName == 'closures':
+        column_names = []
+        cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = " + "'" + tableName + "'")
         column = cursor.fetchone()
-    return Closures(cursor, column_names, tableName)
+        while column is not None:
+            column_names.append(column[0])
+            column = cursor.fetchone()
+        return Closures(cursor, column_names, tableName)
 
 if __name__ == '__main__':
-
     try:
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
-        c = create()
+        c = create_Closures('closures')
         c.random_tree(10)
         conn.commit()
         G = c.getGraph(nx.DiGraph())
-
         plt.title('my_tree')
         pos = graphviz_layout(G, prog='dot')
         nx.draw(G, pos, with_labels=True, arrows=True)
