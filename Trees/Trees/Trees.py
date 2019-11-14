dc5bb0e1ce395e9d54bbd18a5581e17d0bf0273e
Vladislav <33334984+Vladislav3228@users.noreply.github.com>
2019-11-14 15:01:25 +0800

    Update Trees.py

--- Trees/Trees/Trees.py
+++ Trees/Trees/Trees.py
@@ -1,6 +1,6 @@
 from mysql.connector import MySQLConnection, Error
 from NewFolder1.python_mysql_dbconfig import read_db_config
-from Classes.Closures import Closures
+
 import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.drawing.nx_agraph import write_dot, graphviz_layout
@@ -9,22 +9,28 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 dbconfig = read_db_config()
 
-def create_Closures(tableName):
-    if tableName == 'closures':
-        column_names = []
-        cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = " + "'" + tableName + "'")
+def create(tableName):
+    column_names = []
+    cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = " + "'" + tableName + "'")
+    column = cursor.fetchone()
+    while column is not None:
+        column_names.append(column[0])
         column = cursor.fetchone()
-        while column is not None:
-            column_names.append(column[0])
-            column = cursor.fetchone()
+    if tableName == 'closures':
+        from Classes.Closures import Closures
         return Closures(cursor, column_names, tableName)
+    if tableName == 'neighbors':
+        from Classes.Neighbors import Neighbors
+        return Neighbors(cursor, column_names, tableName)
+        
 
 if __name__ == '__main__':
     try:
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
-        c = create_Closures('closures')
+        c = create('neighbors')
         c.random_tree(10)
+
         conn.commit()
         G = c.getGraph(nx.DiGraph())
         plt.title('my_tree')
